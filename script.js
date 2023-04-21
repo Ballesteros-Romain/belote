479eb44830795c2183334c2c584ab9fb9bb85c91
Romain Ballesteros <romain.ballesteros@gmail.com>
2023-04-21 17:52:12 +0200

    modif complete

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ let resultE = document.querySelector(".resultEux");
 let resultN = document.querySelector(".resultNous");
 let valid = document.querySelector(".valid");
 
-valid.addEventListener("click", validation);
+valid.addEventListener("click", validationAndReset);
+numberE.addEventListener("input", updateNumberN);
+numberN.addEventListener("input", updateNumberE);
 
-function validation() {
+function validationAndReset() {
   let valueE = parseInt(numberE.value) || 0;
   let valueN = parseInt(numberN.value) || 0;
 
-  resultE.textContent = parseInt(resultE.textContent) + valueE;
-  resultN.textContent = parseInt(resultN.textContent) + valueN;
+  if (valueE > 160 || valueN > 160) {
+    alert("Les nombres doivent être inférieurs ou égaux à 160.");
+    zero();
+    return; // arrêter la fonction si les nombres sont invalides
+  }
 
   if (isNaN(parseInt(resultE.textContent))) {
     resultE.textContent = 0;
@@ -20,9 +25,66 @@ function validation() {
   if (isNaN(parseInt(resultN.textContent))) {
     resultN.textContent = 0;
   }
+
+  resultE.textContent = parseInt(resultE.textContent) + valueE;
+  resultN.textContent = parseInt(resultN.textContent) + valueN;
+
   if (resultE.textContent >= 1000 || resultN.textContent >= 1000) {
     alert(
       "Vous avez atteint 1000 point, c'est gagné. Sauf si vous allez en 1500"
     );
+  }
+
+  checked();
+  capot();
+  zero();
+  uncheckCheckbox();
+}
+
+function updateNumberN() {
+  let valueE = parseInt(numberE.value);
+  if (!isNaN(valueE)) {
+    numberN.value = (160 - valueE) % 160;
+  }
+}
+
+function updateNumberE() {
+  let valueN = parseInt(numberN.value);
+  if (!isNaN(valueN)) {
+    numberE.value = (160 - valueN) % 160;
+  }
+}
+
+function zero() {
+  numberE.value = "";
+  numberN.value = "";
+}
+
+function checked() {
+  const check1 = document.querySelector("#check1");
+  const check2 = document.querySelector("#check2");
+  if (check1.checked) {
+    resultE.textContent = parseInt(resultE.textContent) + 20;
+  } else if (check2.checked) {
+    resultN.textContent = parseInt(resultN.textContent) + 20;
+  }
+}
+function uncheckCheckbox() {
+  const check1 = document.querySelector("#check1");
+  const check2 = document.querySelector("#check2");
+  check1.checked = false;
+  check2.checked = false;
+  const capot1 = document.querySelector("#capot1");
+  const capot2 = document.querySelector("#capot2");
+  capot1.checked = false;
+  capot2.checked = false;
+}
+function capot() {
+  const capot1 = document.querySelector("#capot1");
+  const capot2 = document.querySelector("#capot2");
+  if (capot1.checked) {
+    resultE.textContent = parseInt(resultE.textContent) + 250;
+  } else if (capot2.checked) {
+    resultN.textContent = parseInt(resultN.textContent) + 250;
   }
 }
